89015f9626a62d02141f8bf0222135a2f3140f90
uunmask2000 <uunmask2000@gmail.com>
2019-04-16 16:12:14 +0800

    add proxy

--- pornhub.py
+++ pornhub.py
@@ -73,9 +73,8 @@ def get_soup(url, c=1):
     # logging.info('get_soup herf' + url)
     # 嚴重錯誤
     if _error_row > 5:
-        # print('嚴重錯誤 大於五次')
-        # os._exit(0)
-        pass
+        print('嚴重錯誤 大於五次')
+        os._exit(0)
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
