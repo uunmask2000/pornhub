fa08f5a4ec2bac1ab5e84d00554b984e4fe5f416
uunmask2000 <uunmask2000@gmail.com>
2019-04-23 11:17:18 +0800

    r

--- Bssoup.py
+++ Bssoup.py
@@ -17,7 +17,7 @@ class Bssoup:
         }
         self.proxies = {}
 
-    def get_soup(self, url, count=0, max_count=10, mode=2,  headers=None, proxies=None):
+    def get_soup(self, url, count=0, max_count=10, mode=2,  headers=None, proxies=None, html=False):
         ###
         if headers == None:
             headers = self.headers
@@ -26,36 +26,40 @@ class Bssoup:
         # ----------------------------------------
         if mode == 1:
             # 代理IP
-            _v = self.get_soup_pu(url, headers, proxies)
+            _v = self.get_soup_pu(url, headers, proxies, html)
         elif mode == 2:
             # 真實IP
-            _v = self.get_soup_pv(url)
+            _v = self.get_soup_pv(url, html)
         ##
         if _v == False:
             count += 1
-            return self.get_soup(url,  count, mode,  headers,  proxies)
+            return self.get_soup(url,  count, mode,  headers,  proxies, html)
         else:
             return _v
 
-    def get_soup_pu(self, url, headers, proxies):
+    def get_soup_pu(self, url, headers, proxies, html_c):
         # 代理ip
         soup = False
         try:
             html_ = requests.get(url, headers=headers,
                                  proxies=proxies, timeout=10)
-            html = html_.content
+            html = html_.content.decode("utf-8")
+            if html_c == True:
+                return html
             soup = BeautifulSoup(html, 'html.parser')
         except:
             pass
             # return False
         return soup
 
-    def get_soup_pv(self, url):
+    def get_soup_pv(self, url, html_c):
         # 真實ip
         soup = False
         try:
             html_ = requests.get(url, timeout=10)
-            html = html_.content
+            html = html_.content.decode("utf-8")
+            if html_c == True:
+                return html
             soup = BeautifulSoup(html, 'html.parser')
         except:
             pass
