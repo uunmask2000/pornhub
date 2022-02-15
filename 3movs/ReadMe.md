284928bc60669bbdb03f602c4a549d687898757b
kk <kk@man.com>
2022-02-15 15:39:02 +0700

    add

---
+++ 3movs/ReadMe.md
@@ -0,0 +1,16 @@
+<!--
+ * @Arthur       : kk
+ * @Date         : 2022-02-15 15:36:55
+ * @LastEditTime : 2022-02-15 15:38:19
+ * @LastEditors  : your name
+ * @Description  : 自動生成 [嚴格紀律 Description]
+ * @FilePath     : /3movs/ReadMe.md
+ * 嚴格紀律
+-->
+# 操作
+
+## 安裝指令
+
+```text
+pip install -r requirements.txt -v
+```
---
+++ 3movs/not_api.py
@@ -0,0 +1,20 @@
+'''
+Arthur       : kk
+Date         : 2022-02-15 15:17:38
+LastEditTime : 2022-02-15 15:34:20
+LastEditors  : your name
+Description  : 自動生成 [嚴格紀律 Description]
+FilePath     : /3movs/not_api.py
+嚴格紀律
+'''
+
+#####
+import re
+import time
+import os
+from urllib.request import urlretrieve
+from urllib.parse import urlparse
+import requests
+import json
+
+from bs4 import BeautifulSoup
